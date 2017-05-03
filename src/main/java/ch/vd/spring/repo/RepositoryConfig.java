5980f23d03b141d4845be70990547dee5c06215b
C. Mourad Jaber <cmjaber@nativobject.com>
2017-05-03 15:39:21 +0200

    clean

--- src/main/java/ch/vd/spring/repo/RepositoryConfig.java
+++ src/main/java/ch/vd/spring/repo/RepositoryConfig.java
@@ -1,6 +1,5 @@
 package ch.vd.spring.repo;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -8,6 +7,5 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
 @EnableJpaRepositories
 @EntityScan(basePackages = "ch.vd.spring.model")
-@EnableAutoConfiguration
 public class RepositoryConfig {
 }
