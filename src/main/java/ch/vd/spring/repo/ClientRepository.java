3d0afa48ef69590229cf58d57c2fbd0eb991ec22
C. Mourad Jaber <cmjaber@nativobject.com>
2017-05-03 14:55:15 +0200

    ajout du repo

---
+++ src/main/java/ch/vd/spring/repo/ClientRepository.java
@@ -0,0 +1,8 @@
+package ch.vd.spring.repo;
+
+import ch.vd.spring.model.Client;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+
+public interface ClientRepository extends PagingAndSortingRepository<Client, Integer> {
+}
